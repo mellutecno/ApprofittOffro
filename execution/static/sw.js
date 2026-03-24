926670099450d1ea503a41ad7b50b0c91c048543
ApprofittOffro Dev <dev@approfittoffro.local>
2026-03-25 00:30:55 +0100

    Make landing nav a collapsible toast menu

--- execution/static/sw.js
+++ execution/static/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'approfittoffro-v102';
+const CACHE_NAME = 'approfittoffro-v103';
 const ASSETS = [
     '/',
     '/dashboard',
-  '/static/css/styles.css?v=55',
+  '/static/css/styles.css?v=56',
     '/static/js/app.js',
     '/static/img/approfitto.png?v=2',
     '/static/img/hero.png',
