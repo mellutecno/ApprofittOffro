69f15cf536afd231954004c1615f7ef966bbb4b7
ApprofittOffro Dev <dev@approfittoffro.local>
2026-04-16 08:11:08 +0200

    Fix biometric auth: FlutterFragmentActivity

--- mobile_app/android/app/build.gradle.kts
+++ mobile_app/android/app/build.gradle.kts
@@ -64,7 +64,7 @@ android {
         create("play") {
             dimension = "distribution"
             applicationId = "com.mellutecno.approfittoffro"
-            versionCode = 3009
+            versionCode = 3010
         }
     }
 
--- mobile_app/android/app/src/main/kotlin/com/mellutecno/approfittoffro/MainActivity.kt
+++ mobile_app/android/app/src/main/kotlin/com/mellutecno/approfittoffro/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.mellutecno.approfittoffro
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
