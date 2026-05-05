b47af57c06e18989c8eb0d4b3f1cd309062c225d
ApprofittOffro Dev <dev@approfittoffro.local>
2026-05-05 16:51:51 +0200

    Bump Play Store version code to 3043

--- mobile_app/android/app/build.gradle.kts
+++ mobile_app/android/app/build.gradle.kts
@@ -60,7 +60,7 @@ android {
         create("play") {
             dimension = "distribution"
             applicationId = "com.mellutecno.approfittoffro"
-            versionCode = 3042
+            versionCode = 3043
             ndk {
                 abiFilters.add("arm64-v8a")
             }
