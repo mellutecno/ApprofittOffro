163fe8bc20f8e07d020bb0bdc881b92f6a5292e7
ApprofittOffro Dev <dev@approfittoffro.local>
2026-05-08 15:09:56 +0200

    Bump Play version code to 3048

--- mobile_app/android/app/build.gradle.kts
+++ mobile_app/android/app/build.gradle.kts
@@ -60,7 +60,7 @@ android {
         create("play") {
             dimension = "distribution"
             applicationId = "com.mellutecno.approfittoffro"
-            versionCode = 3047
+            versionCode = 3048
             ndk {
                 abiFilters.add("arm64-v8a")
             }
